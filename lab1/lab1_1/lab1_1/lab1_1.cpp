38de428f89513ebe8c1561006bcf41b5a974696a
Aryna Shablii <aryna1201shablii@gmail.com>
2026-09-15 16:50:02 +0300

    lab 1 added

---
+++ lab1/lab1_1/lab1_1/lab1_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "UKR");
+    int x, y;
+
+    cout << "Введiть x: ";
+    cin >> x;
+
+    if (x > -10)
+    {
+        if (x <= -5)
+        {
+            y = x * x * x - 6;
+            cout << "y = " << y;
+        }
+        else if (x > 5)
+        {
+            if (x <= 15)
+            {
+                y = x * x * x - 6;
+                cout << "y = " << y;
+            }
+            else if (x >= 25)
+            {
+                y = 2 * x * x * x - 3 * x + 2;
+                cout << "y = " << y;
+            }
+            else
+                cout << "Error: x не входить в область визначення";
+        }
+        else
+            cout << "Error: x не входить в область визначення";
+    }
+    else
+        cout << "Error: x не входить в область визначення";
+    return 0;
+}
---
+++ lab1/lab1_2/lab1_2/lab1_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "UKR");
+    int x, y;
+
+    cout << "Введiть x: ";
+    cin >> x;
+
+    if ((x > -10 && x <= -5) || (x > 5 && x <= 15)) {
+        y = x * x * x - 6;
+        cout << "y = " << y;
+    }
+    else if (x >= 25)
+    {
+        y = 2 * x * x * x - 3 * x + 2;
+        cout << "y = " << y;
+    }
+    else
+        cout << "x не входить в область визначення";
+    return 0;
+}
